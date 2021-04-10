37d34dc371653d7ca1be7b9f275e9546382ab636
Pedro Bronsveld <pedrobronsveld@gmail.com>
2021-04-11 00:29:38 +0200

    Add global set of entities and default entity constructor.

--- src/models.ts
+++ src/models.ts
@@ -1,7 +1,17 @@
-// 
+/**
+ * Set of all entities in the project.
+ * When a new entity is added to the project,
+ * it also has to be added to this list.
+ */
+interface Entities {
+    Student: Student,
+    Course: Course,
+    Grades: Grades,
+    Lecture: Lecture
+}
+
 // Entities
 interface Student {
-    kind: "Student";
     name: string;
     surname: string;
     grades: Grades[];
@@ -9,28 +19,58 @@ interface Student {
 }
 
 interface Course {
-    kind: "Course";
     name: string;
     studyPoints: number;
     lectures: Lecture[];
 }
 
 interface Grades {
-    kind: "Grades";
     grade: number;
     course: Course;
 }
 
 interface Lecture {
-    kind: "Lecture";
     title: string;
     topic: string;
 }
-
-type Entity = Student | Course | Grades | Lecture;
 
 // Renderer
 interface Rendered {
     student: Student[];
     courses: Course[];
+}
+
+/**
+ * Gets the default 'empty' version of an entity.
+ * When a new entity is added to the global `Entities` set, the developer is
+ * forced by Typescript to also implement a default object for that entity in 
+ * the `defaults` object in this function.
+ * 
+ * @summary Returns the default version of an entity.
+ * @param name The name of the entity for which to retrieve the default object.
+ * @returns A default entity object corresponding to the type `name`
+ */
+const defaultEntity = <Name extends keyof Entities>(name: Name) : Entities[Name] => {
+    const defaults: Entities = {
+        Student: {
+            name: "",
+            surname: "",
+            grades: [],
+            courses: []
+        },
+        Course: {
+            name: "",
+            studyPoints: 0,
+            lectures: []
+        },
+        Grades: {
+            grade: 0,
+            course: defaultEntity("Course")
+        },
+        Lecture: {
+            title: "",
+            topic: ""
+        }
+    }
+    return defaults[name];
 }
