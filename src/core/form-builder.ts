722c065b92e86eba431d30049c87c96c3c24c594
Albert <0961988@hr.nl>
2021-05-21 13:47:40 +0200

    Added documentation to the FormBuilder

--- src/core/form-builder.ts
+++ src/core/form-builder.ts
@@ -2,19 +2,32 @@ import { IsEqual, Listify } from "../utilities/helper-types";
 import { defaultEntity, Entities } from "./entities";
 import { Queryable, QueryFunction, queryable } from "./queryable";
 
+
+/**
+ * Type definition for the `FormBuilder`, the root object of the project.
+ */
 interface FormBuilder<Spec extends Listify<Partial<Entities>>> {
     readonly specification: Listify<Partial<Entities>>,//Partial<Entities>,
     entity: <EQ extends keyof Entities>(entity: EQ, 
-//        q: QueryFunction<Entities[EQ], {}, Partial<Entities[EQ]>, Partial<Entities[EQ]> >) =>
         q: QueryFunction<Entities[EQ], Partial<Entities[EQ]>, Partial<Entities[EQ]>, Partial<Entities[EQ]> >) =>
         FormBuilder<Spec>
 }
 
-// FormBuilder constructor
-const FormBuilderStep = <Spec extends Listify<Partial<Entities>>>(specification: Spec) : FormBuilder<Spec> => ({
+/**
+ * Type definition for the `FormBuilder`, the root object of the project.
+ * 
+ * @param specification The specification the build step shoud be initialized with. 
+ * @returns A new `FormBuilderStep` instance.
+ */
+ const FormBuilderStep = <Spec extends Listify<Partial<Entities>>>(specification: Spec) : FormBuilder<Spec> => ({
         specification: specification,        
         entity: (entity, q) =>  FormBuilderStep({...specification, ...{[entity]: [q(queryable(defaultEntity(entity))).querried]} })
     })
 
-export const FormBuilder = (() => FormBuilderStep({}))()
+/**
+ * `FormBuilder` instance, creating a new and empty FormBuilder, ready to use.
+ * 
+ * @returns A new `FormBuilder` instance.
+ */
+ export const FormBuilder = (() => FormBuilderStep({}))()
 
