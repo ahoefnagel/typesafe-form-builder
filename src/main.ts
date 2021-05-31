8101adbc7f0ebdfd2c63e86d55a6ec338eb140f1
Joey Ouwens <joey.ouwens@gmail.com>
2021-05-31 15:44:27 +0200

    render functions moved to different file

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { defaultEntity } from "./core/entities";
 import { queryable } from "./core/queryable";
 import {Renderer, RenderFunctions} from "./core/renderer";
 import {getKeys} from "./utilities/object-utilities";
+import {renderToHtml, renderToString} from "./utilities/render-utilities";
 
 // testing queryable
 // const testStudent = defaultEntity("Student");
@@ -72,46 +73,8 @@ let testObject = {
   ]
 }
 
-const createDate = function(date:Date):string {
-  const month = (date.getMonth()+1) < 10? "0" + (date.getMonth()+1) : date.getMonth()+1
-  return `${date.getFullYear()}-${month}-${date.getDate()}`
-}
-
-
-const renderToString:RenderFunctions<string> = {
-  string: function <K>(key:K , value: string):string {return `${key}: ${value}`},
-  number: function <K>(key:K, value: number):string { return `${key}: ${value}`},
-  boolean: function  <K>(key: K, value: boolean):string { return `${key}: ${value}`},
-  date: function  <K>(key: K, value: Date):string { return `${key}: ${createDate(value)}`},
-  object: function <T,K>(key: K, value:T):string {
-    let res = ""
-    getKeys(value).forEach(key => res += '\n' + value[key])
-    return res
-  },
-  array: function <T,K>(key: K, value:T[]):string {
-    let res = ""
-    value.forEach(v => res += v)
-    return `${key} ${res}`}
-}
-
-const renderToHtml:RenderFunctions<string> = {
-  string: function <K>(key:K , value: string):string {return `<p><label>${key}</label><input type="text" id="${key}" value="${value}"</p>`},
-  number: function <K>(key:K, value: number):string { return `<p><label>${key}</label><input type="number" id="${key}" value="${value}"</p>`},
-  boolean: function  <K>(key: K, value: boolean):string { return `<p><label>${key}</label><input type="checkbox" id="${key}" value="${value}"</p>`},
-  date: function  <K>(key: K, value: Date):string { return `<p><label>${key}</label><input type="date" id="${key}" value="${createDate(value)}"</p>`},
-  object: function <T,K>(key: K, value:T):string {
-    let res = "<li>"
-    getKeys(value).forEach(key => res +=  `<ul> ${value[key]}</ul>`)
-    return res + "</li>"
-  },
-  array: function <T,K>(key: K, value:T[]):string {
-    let res = ""
-    value.forEach(v => res += v)
-    return `<p>${key}</p>${res}`}
-}
-let renderTest = Renderer(testObject)
-let renderedObject = renderTest.render(renderToHtml)
-renderedObject.forEach(value => console.log(value))
+Renderer(testObject).render(renderToHtml).forEach(value => console.log(value))
+Renderer(testObject).render(renderToString).forEach(value => console.log(value))
 
 // FormBuilder({...data, value: newValue}).render(callback)
 
