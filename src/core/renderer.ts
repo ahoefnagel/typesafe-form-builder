8d83ecf5e143652ff7fb809784616081bffcb0a2
Joey Ouwens <joey.ouwens@gmail.com>
2021-05-31 17:38:04 +0200

    Comments added

--- src/core/renderer.ts
+++ src/core/renderer.ts
@@ -1,5 +1,9 @@
 import {Entities, Rendered} from "./entities";
 import {customTypeOf, extendTypeOf, getKeys} from "../utilities/object-utilities";
+/**
+ * Renderer Functions Interface
+ * Callbacks are used for talking with the render function.
+ */
 export type RenderFunctions<T> = {
   string:  <K>(key: K, value: string) => T,
   number: <K>(key: K, value: number) => T
@@ -8,12 +12,21 @@ export type RenderFunctions<T> = {
   array: <U,K>(key: K, value: U[]) => T
   object: <U,K>(key: K, value: U) => T
 
-} 
+}
+
+/**
+ * Renderer Interface
+ */
 type Renderer<RenderData extends Object> = {
   data: RenderData
   render: <RenderOutput>(callback: RenderFunctions<RenderOutput>) => RenderOutput[]
 }
+/**
+ * Renderer function, creates a renderer object.
+ *  @param data  - Contains the specification and its data.
+ *  @param render - Render function for the renderer object, needs a callback based on the RenderFunctions interface. Will return all info from data and pass it to their respective callbacks.
 
+ */
 export const Renderer = <RenderData>(data:RenderData): Renderer<RenderData> => {
   return {
     data: data,
@@ -33,11 +46,10 @@ export const Renderer = <RenderData>(data:RenderData): Renderer<RenderData> => {
           return callback.date(key,value);
         }
         else if (customTypeOf(value, "array")) {
-          return callback.array(key,  Renderer(value).render(callback));
+            return callback.array(key,  Renderer(value).render(callback));
         }
-        else if (customTypeOf(value, "object")){
-          return callback.object(key, Renderer(value).render(callback));
-
+        else if (customTypeOf(value, "object")){ 
+            return callback.object(key, Renderer(value).render(callback));
         }
         else
             throw "Unsupported type"
--- src/utilities/render-utilities.ts
+++ src/utilities/render-utilities.ts
@@ -6,14 +6,16 @@ const createDate = function(date:Date):string {
   return `${date.getFullYear()}-${month}-${date.getDate()}`
 }
 
-
+/**
+ * Callbacks to be used with render, will put all data from renderer into string
+ */
 export const renderToString:RenderFunctions<string> = {
   string: function <K>(key:K , value: string):string {return `${key}: ${value}`},
   number: function <K>(key:K, value: number):string { return `${key}: ${value}`},
   boolean: function  <K>(key: K, value: boolean):string { return `${key}: ${value}`},
   date: function  <K>(key: K, value: Date):string { return `${key}: ${createDate(value)}`},
   object: function <T,K>(key: K, value:T):string {
-    let res = ""
+    let res = key + ""
     getKeys(value).forEach(key => res += '\n' + value[key])
     return res
   },
@@ -23,15 +25,19 @@ export const renderToString:RenderFunctions<string> = {
     return `${key} ${res}`}
 }
 
+
+/**
+ * Callbacks to be used with render, will put all data from renderer into HTML format
+ */
 export const renderToHtml:RenderFunctions<string> = {
   string: function <K>(key:K , value: string):string {return `<p><label>${key}</label><input type="text" id="${key}" value="${value}"</p>`},
   number: function <K>(key:K, value: number):string { return `<p><label>${key}</label><input type="number" id="${key}" value="${value}"</p>`},
   boolean: function  <K>(key: K, value: boolean):string { return `<p><label>${key}</label><input type="checkbox" id="${key}" value="${value}"</p>`},
   date: function  <K>(key: K, value: Date):string { return `<p><label>${key}</label><input type="date" id="${key}" value="${createDate(value)}"</p>`},
   object: function <T,K>(key: K, value:T):string {
-    let res = "<li>"
-    getKeys(value).forEach(key => res +=  `<ul> ${value[key]}</ul>`)
-    return res + "</li>"
+    let res = key + "<ul>"
+    getKeys(value).forEach(key => res +=  `<li> ${value[key]}</li>`)
+    return res + "</ul>"
   },
   array: function <T,K>(key: K, value:T[]):string {
     let res = ""
