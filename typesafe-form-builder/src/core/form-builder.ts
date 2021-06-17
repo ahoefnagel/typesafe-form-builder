c2032b83109d8f26b2e55e3920786db278505e2f
Pedro Bronsveld <pedrobronsveld@gmail.com>
2021-06-17 23:24:35 +0200

    Add doc string to entity function.

--- typesafe-form-builder/src/core/form-builder.ts
+++ typesafe-form-builder/src/core/form-builder.ts
@@ -6,6 +6,23 @@ import { QueryFunction, queryable } from "./queryable";
  */
  interface FormBuilder<Spec> {
     readonly specification: Spec,
+
+    /**
+     * @param entity name of the entity to be queried.
+     * Entities are defined in the `Entities` interface in `Entities.ts`.
+     * @param q querry function to perform query operations on the specified entity.
+     * @param data optional data object of the given `entity`.
+     * If no data is provided a new object is created using the `defaultEntity` function.
+     * @returns a new `FormBuilder` instance with the given `entity` added to the `specification`.
+     * 
+     * Example usage:
+     * ```
+     * FormBuilder.entity("Student", 
+     * q => q.select("name", "surname").children("grades", 
+     * q => q.select("grade").child("course", 
+     * q => q.select("name"))))
+     * ```
+     */
     entity: <EQ extends keyof Omit<Entities, keyof Spec>, OutputObject, OutputQuerried, 
         QF extends QueryFunction<Entities[EQ], {}, OutputObject, OutputQuerried>>
         (entity: EQ, q: QF, data?: Entities[EQ]) =>
