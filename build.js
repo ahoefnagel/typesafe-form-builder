2db676b2d2f690ded2c09053809b625938689f4a
Albert <0961988@hr.nl>
2021-06-22 21:25:36 +0200

    Added commands for auto building

---
+++ build.js
@@ -0,0 +1,6 @@
+const fs = require("fs-extra");
+
+fs.rmdirSync("build", {recursive: true});
+
+fs.copySync("angular-form-builder/dist/angular-form-builder", "build/angular");
+// fs.copySync("react/dist", "build/angular");
