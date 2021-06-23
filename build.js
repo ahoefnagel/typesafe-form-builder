22e157a5e07997e4df15056f9bd31d9fec69bf98
Pedro Bronsveld <pedrobronsveld@gmail.com>
2021-06-23 21:19:37 +0200

    Add react to build and install scripts.

--- build.js
+++ build.js
@@ -6,7 +6,7 @@ fs.rmdirSync("build", {recursive: true});
 fs.copySync("angular-form-builder/dist/angular-form-builder", "build/angular");
 
 // Copy all of the react sample code into the build/react folder. 
-// fs.copySync("react/dist", "build/react");
+fs.copySync("react-form-builder/build", "build/react");
 
 // Copy the relevant pages data into the build folder
 fs.copySync("documentation/pages", "build");
